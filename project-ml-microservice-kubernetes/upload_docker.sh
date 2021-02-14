2f6d9104141aa2fa0846ef9223451dc327cb066f
ConorMcElvogue <conormcelvogue@gmail.com>
2021-02-14 22:21:44 +0000

    uploading project

--- project-ml-microservice-kubernetes/upload_docker.sh
+++ project-ml-microservice-kubernetes/upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath="cmcelvogue/udacityapizip"
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
 
+docker login -u "cmcelvogue" -p "Ballerin_123!!"
+docker tag udacityapizip $dockerpath
+
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
